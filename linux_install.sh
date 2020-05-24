1bf9940686affb7c80f528a22673e0598a8e38b0
mhbahmani <mhbahmani79@gmail.com>
2020-05-24 17:55:27 +0430

    make some changes in linux installation script

--- linux_install.sh
+++ linux_install.sh
@@ -1,3 +1,4 @@
+mkdir Watermark\ Adder/
 echo installing virtualenv
 
 python3 -m pip install --user virtualenv
@@ -6,6 +7,12 @@ python3 -m pip install --user virtualenv
 echo cloning repository and install requirements
 
 git clone https://github.com/mhbahmani/Watermark-Adder
+mv Watermark-Adder/main.py ./
+mv Watermark-Adder/LICENCE ./
+mv Watermark-Adder/requirements.txt ./
+mv Watermark-Adder/README.md ./
+rm -rf Watermark-Adder
+
 virtualenv -p python3 venv
 source venv/bin/activate
 pip install -r requirements.txt
