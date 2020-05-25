f9b34d84c92f6d12f0a31032980cb5d83f5bb4d0
mhbahmani <mhbahmani79@gmail.com>
2020-05-25 21:48:44 +0430

    change main.py to watermark

--- linux_install.sh
+++ linux_install.sh
@@ -7,7 +7,8 @@ python3 -m pip install --user virtualenv
 echo cloning repository and install requirements
 
 git clone https://github.com/mhbahmani/Watermark-Adder
-mv Watermark-Adder/main.py ./
+mv Watermark-Adder/Watermark ./
+chmod +x watermark
 mv Watermark-Adder/LICENCE ./
 mv Watermark-Adder/requirements.txt ./
 mv Watermark-Adder/README.md ./
