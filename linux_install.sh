76ed19d2ec3fb84af4a118ade52e47f37478e03d
mhbahmani <mhbahmani79@gmail.com>
2020-05-30 20:03:20 +0430

    improve install script

--- linux_install.sh
+++ linux_install.sh
@@ -3,7 +3,6 @@ echo installing virtualenv
 
 python3 -m pip install --user virtualenv
 
-
 echo cloning repository and install requirements
 
 git clone https://github.com/mhbahmani/Watermark-Adder
@@ -13,6 +12,17 @@ mv Watermark-Adder/LICENCE ./
 mv Watermark-Adder/requirements.txt ./
 mv Watermark-Adder/README.md ./
 rm -rf Watermark-Adder
+
+script_path=/usr/local/bin/watermark
+sudo tee $script_path << EOF > /dev/null
+#!/usr/bash
+PATH=~/Codes/Python/watermark
+
+source $PATH/venv/bin/activate
+python watermark.py "$@"
+EOF
+
+sudo chmod +x $script_path
 
 virtualenv -p python3 venv
 source venv/bin/activate
